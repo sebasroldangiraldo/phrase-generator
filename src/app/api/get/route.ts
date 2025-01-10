6646f80fb549e37668497756434e823827310c91
sebasroldangiraldo <sebastianroldangiraldo@gmail.com>
2025-01-10 14:10:17 -0500

    add backend logic and  API route

---
+++ src/app/api/get/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+
+export async function GET(_request: Request) {
+
+    const subjects : string[] = ["The dog", "The turtle", "My friend", "Sebastián"];
+    const predicates : string[] = ["runs fast", "is very wise", "loves coding", "sings poorly", 'hates coding'];
+
+    const subject = subjects[Math.floor(Math.random() * subjects.length)];
+    const predicate = predicates[Math.floor(Math.random() * predicates.length)];
+
+    let phrase = `${subject} ${predicate}`;
+
+    if (subject.length > 8) {
+        phrase = `${subject} intelligent ${predicate}`
+    };
+
+    if (predicate.includes('coding')) {
+        phrase = `${subject} ${predicate}!`
+    };
+
+    return NextResponse.json({
+        phrase
+    });
+}
