3d11a4d9318fb48b8c77069065eec5ce74531ed2
sebasroldangiraldo <sebastianroldangiraldo@gmail.com>
2025-01-10 19:05:34 -0500

    add styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,20 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Phrase Generator",
-  description: "Sebastián Roldán Giraldo",
+    title: "Phrase Generator",
+    description: "Sebastián Roldán Giraldo",
 };
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['300', '400', '600']});
+const poppins = Poppins({ subsets: ['latin'], weight: ['300', '400', '600'] });
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en">
-      <body className={poppins.className}>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={poppins.className}>
+                {children}
+            </body>
+        </html>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,36 @@ import { useState } from "react";
 
 export default function Home() {
 
-  const [phrase, setPhrase] = useState('');
+    const [phrase, setPhrase] = useState('');
 
-  const handleClick = async () => {
+    const handleClick = async () => {
 
-    try {
+        try {
 
-      const response = await fetch('/api/get');
-      const data = await response.json();
+            const response = await fetch('/api/get');
+            const data = await response.json();
 
-      setPhrase(data.phrase);
-      
-    } catch (error) {
-      console.log(error);
-    }
+            setPhrase(data.phrase);
 
-  };
+        } catch (error) {
+            console.log(error);
+        }
 
-  return (
-    <div>
-      <div>Home</div>
-      <button onClick={handleClick}>Search</button>
-      <p>{phrase}</p>
-    </div>
+    };
 
-  );
+    return (
+        <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center p-8">
+       
+            <h1 className="text-4xl font-bold text-yellow-400 mb-6">Phrase Generator Challenge</h1>
+
+            <div className="flex items-center space-x-4">
+                
+                <div className="bg-gray-800 text-white p-2 rounded-lg shadow-xl w-full sm:w-96 flex justify-center">
+                    <p className="text-base text-center font-light">{phrase || 'No phrase generated yet'}</p>
+                </div>
+
+                <button onClick={handleClick} className="bg-yellow-500 text-gray-900 py-2 px-4 rounded-lg hover:bg-yellow-600 transition">Play</button>
+            </div>
+        </div>
+    );
 }
